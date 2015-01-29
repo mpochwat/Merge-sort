54234d6ce0476bf3744b8b001a84d5cccf7ed224
Martin <martinochwat1@gmail.com>
2015-01-29 15:42:09 -0500

    update

--- mergesort.rb
+++ mergesort.rb
@@ -1,16 +1,16 @@
 def merge_sort(arr)
-	length = arr.length
-	return arr if length == 1
-	a = arr[0..length/2-1]
-	b = arr[length/2..length]
-	merge( merge_sort(a), merge_sort(b) )
+  length = arr.length
+  return arr if length == 1
+  a = arr[0..length/2-1]
+  b = arr[length/2..length]
+  merge( merge_sort(a), merge_sort(b) )
 end
 
 def merge(a, b, sorted=[])
-	while !a.empty? && !b.empty?
-		sorted << ( a[0] < b[0] ? a.shift : b.shift )
-	end	
-	sorted.concat(a).concat(b)
+  while !a.empty? && !b.empty?
+    sorted << ( a[0] < b[0] ? a.shift : b.shift )
+  end	
+  sorted.concat(a).concat(b)
 end
 
 p merge_sort([1])
